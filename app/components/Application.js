7566fca7bebd58614ded665b86aaa0137c9bc0ec
Ikhsan Assaat <ixnixnixn@yahoo.com>
2015-11-28 11:41:36 +0000

    rendering all components

--- app/components/Application.js
+++ app/components/Application.js
@@ -2,14 +2,25 @@
 
 import React from 'react'
 import MapComponent from './maps/MapComponent'
+import Statistic from './Statistic'
 
 class Application extends React.Component {
-  render() {
-    return (
-      <MapComponent />
-    )
-  }
+    render() {
 
+        return (
+            <div>
+                <div className="row">
+                    <h3>Statistik</h3>
+                    <Statistic />
+                </div>
+
+                <div className="row">
+                    <h3>Map</h3>
+                </div>
+                <MapComponent />
+            </div>
+        )
+    }
 }
 
 export default Application
