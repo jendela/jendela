b7c86709d862214aac49b3440d3b5ba844cb353b
Ikhsan Assaat <ixnixnixn@yahoo.com>
2015-12-01 05:15:14 +0000

    another attemp to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
 const webpack = require('webpack')
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const isLive = (process.env.NODE_ENV === 'production' | process.env.NODE_ENV === 'staging')
+
+const sassLoaders = [
+    'css-loader',
+    'postcss-loader',
+    'sass-loader?'
+    + '&includePaths[]=' + path.resolve(__dirname, './bower_components/foundation-sites/scss')
+    // + '&includePaths[]=' + path.resolve(__dirname, './bower_components/motion-ui/src')
+];
 
 module.exports = {
   entry: "./app/app.jsx",
@@ -10,21 +20,40 @@ module.exports = {
     publicPath: "/assets/"
   },
 
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/],
+        loaders: ["babel?presets[]=airbnb"]
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader!css-loader')
+      }
+
+    ],
+  },
+
   plugins: [
+
+    new ExtractTextPlugin(path.resolve(__dirname, 'public/___.css'), {
+        allChunks: true
+    }),
+
     // Using webpack with shims and polyfills: http://mts.io/2015/04/08/webpack-shims-polyfills/
     new webpack.ProvidePlugin({
       'fetch': 'imports?this=>global!exports?global.fetch!whatwg-fetch'
     })
   ],
 
-  module: {
-    loaders: [
-      {
-        test: [/\.jsx$/, /\.js$/],
-        exclude: [/node_modules/],
-        loaders: (isLive ? ["babel?presets[]=airbnb"] : ["react-hot", "babel?presets[]=airbnb"])
-      }
-    ],
-  },
+  resolve: {
+      extensions: ['', '.js', '.jsx', '.scss'],
+      modulesDirectories: ['app', 'node_modules', 'bower_components']
+  }
 
 }
