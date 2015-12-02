f2a89f71ea487bceb164c768ab2e185276e8f787
Alsasian Atmopawiro <alsasian@gmail.com>
2015-12-02 01:17:44 +0100

    add faq page

--- app/components/service/Service.js
+++ app/components/service/Service.js
@@ -5,6 +5,24 @@ import { Link } from 'react-router'
 
 import StringConstants from '../../constants/StringConstants'
 
+const styles = {
+    info: {
+        background: "#9DBBD0",
+        paddingTop: "25px",
+        paddingBottom: "25px"
+    },
+    title: {
+        fontSize: "2em",
+        fontWeight: "bold"
+    },
+    content: {
+        paddingTop: "25px",
+        paddingBottom: "25px"
+    },
+    entry: {
+        paddingBottom: "10px"
+    }
+}
 class Service extends React.Component {
 
     constructor(props) {
@@ -25,20 +43,34 @@ class Service extends React.Component {
 
     render() {
         return (
-            <section className="row">
-                <div className="small-12 columns">
-                    <h3>{StringConstants.SERVICE}</h3>
-                </div>
-                <div className="small-12 columns">
-                    {this.state.services.map((service)=> {
-                        return (
-                            <Link to={"/services/"+service.id} className="button">{service.get("name")}</Link>
-                        );
-                    })}
-                </div>
-                <div className="small-6 medium-3 columns">
-                </div>
-            </section>
+            <div>
+                <section style={styles.info}>
+                    <div className="row">
+                        <div className="small-12 columns">
+                            <div style={styles.title}>Layanan Publik</div>
+
+                            <section>
+                                <div className="row">
+                                    <div className="large-12 columns">
+                                        Berikut ini layanan publik di Indonesia
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </section>
+                <section style={styles.content} className="row">
+                    <div className="small-12 columns">
+                        {this.state.services.map((service)=> {
+                            return (
+                                <Link to={"/services/"+service.id} className="button">{service.get("name")}</Link>
+                            );
+                        })}
+                    </div>
+                    <div className="small-6 medium-3 columns">
+                    </div>
+                </section>
+            </div>
         );
     }
 }
