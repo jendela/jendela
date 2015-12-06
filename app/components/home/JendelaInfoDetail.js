5fa184f342b8f5635680b930cbbf0a78b7a8e9aa
Ikhsan Assaat <ixnixnixn@yahoo.com>
2015-12-06 12:00:09 +0000

    emphasize questions #88

--- app/components/home/JendelaInfoDetail.js
+++ app/components/home/JendelaInfoDetail.js
@@ -23,17 +23,20 @@ class JendelaInfoDetail extends React.Component {
                         <div className="small-12 medium-4 columns">
                             <h5 style={styles.points}>Menulis Ulasan</h5>
 
-                            <p>Anda diminta uang suap ketika membuat KTP? Anda bertemu dengan pejabat baik di kantor kecamatan? Ceritakan pengalaman Anda di jendela agar bersama-sama kita bisa meningkatkan transparansi layanan publik</p>
+                            <p><em>Anda diminta uang suap ketika membuat KTP? Anda bertemu dengan pejabat baik di kantor kecamatan?</em></p>
+                            <p>Ceritakan pengalaman Anda di jendela agar bersama-sama kita bisa meningkatkan transparansi layanan publik</p>
                         </div>
                         <div className="small-12 medium-4 columns">
                             <h5 style={styles.points}>Melihat Visualisasi Data Layanan Publik Di Indonesia</h5>
 
-                            <p>Berapa rata-rata penduduk kota Solo harus mengeluarkan uang untuk membuat KTP (yang seharusnya gratis)? Di provinsi mana bisa didapati laporan uang suap paling banyak? Jendela menggabungkan semua laporan masyarakat yang masuk dan menampilkannya dalam bentuk visualisasi yang apik.</p>
+                            <p><em>Berapa rata-rata penduduk kota Solo harus mengeluarkan uang untuk membuat KTP (yang seharusnya gratis)? Di provinsi mana bisa didapati laporan uang suap paling banyak?</em></p>
+                            <p>Jendela menggabungkan semua laporan masyarakat yang masuk dan menampilkannya dalam bentuk visualisasi yang apik.</p>
                         </div>
                         <div className="small-12 medium-4 columns">
                             <h5 style={styles.points}>Membaca Informasi Mengenai Layanan Publik</h5>
 
-                            <p>Bagaimana prosedur pembuatan akta nikah? Berapa biaya pembuatan SIM A? Informasi penting mengenai layanan publik ini juga bisa Anda dapatkan di jendela</p>
+                            <p><em>Bagaimana prosedur pembuatan akta nikah? Berapa biaya pembuatan SIM A?</em></p>
+                            <p>Informasi penting mengenai layanan publik ini juga bisa Anda dapatkan di jendela</p>
                         </div>
                     </div>
                 </div>
